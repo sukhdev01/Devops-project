071ffa581ff961324e98b5b1e782c649231cfb0b
bb-sukhdevsingh <sukhdev.singh22@bigbasket.com>
2025-01-24 00:14:01 +0530

    Basic main function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sukhdev01/Devops-project
+
+go 1.23.3
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-# main.go file. 
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	message := "Welcome to Go..."
+	fmt.Fprint(w, message)
+}
+
+func main() {
+
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Starting server on :3456...")
+	err := http.ListenAndServe(":3456", nil)
+
+	if err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+
+}
